a273fb06a8b87277ce0d8f132718774866832fda
Adurotimi Joshua Steven <adurotimijoshua@gmail.com>
2023-03-04 10:32:21 +0100

    auth

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
